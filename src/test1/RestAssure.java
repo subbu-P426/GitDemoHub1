725dd465f0391c0d8c7bacf50c2b2f6df8e59c99
Subbu <subbu426@gmail.com>
2020-08-22 15:47:04 +0530

    Asian Time Zone Changes

--- src/test1/RestAssure.java
+++ src/test1/RestAssure.java
@@ -5,6 +5,11 @@ public class RestAssure {
 	{
 		System.out.println("ResAssured  Test");
 	}
+	
+	public void getdataNewDev()
+	{
+		System.out.println("ResAssured passed Test newly");
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
